05115773df653f6b450e830c090044bf99b676e8
Mukul Sharma <muku0784@gmail.com>
2025-11-27 22:30:21 +0530

    Create url_mappings.py

---
+++ url_mappings.py
@@ -0,0 +1,9 @@
+PAGE_TO_URL = {
+    "employee_workspace": "agento",
+    "ai_call_mode": "call-mode",
+    "admin_dashboard": "dashboard",
+    "user_profile": "profile",
+    "auth": "login"
+}
+URL_TO_PAGE = {v: k for k, v in PAGE_TO_URL.items()}
+
